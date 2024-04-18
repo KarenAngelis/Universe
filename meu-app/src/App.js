81727b2098aad5a405a063adb265e5f7fce1c572
Karen <karen.denich@gmail.com>
2024-04-18 10:12:55 -0300

    Avançando no CSS

---
+++ meu-app/src/App.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
+import Home from './components/Home/Home';
+import Universe from './components/Universe/Universe';
+import Exploration from './components/Exploration/Exploration';
+import Header from './components/Header/Header';
+import './App.css'; // Seus estilos globais
+import homeBackground from './assets/mountains-universe-1.png';
+import universeBackground from './assets/mountains-universe02.png';
+import explorationBackground from './assets/mountains-universe-3.png';
+
+function App() {
+  return (
+    <Router>
+      <PageLayout />
+    </Router>
+  );
+}
+
+function PageLayout() {
+  const location = useLocation();
+  const backgroundImages = {
+    '/': homeBackground,
+    '/universe': universeBackground,
+    '/exploration': explorationBackground
+  };
+
+  const currentBackground = backgroundImages[location.pathname] || homeBackground;
+
+  const appStyle = {
+    backgroundImage: `url(${currentBackground})`,
+    backgroundSize: 'cover',
+    minHeight: '100vh',
+    width: '100vw'
+  };
+
+  return (
+    <div style={appStyle}>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/universe" element={<Universe />} />
+        <Route path="/exploration" element={<Exploration />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
+
---
+++ meu-app/src/components/Home/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div>
+      <h1>Explore o universo</h1>
+      <button>QUERO SABER MAIS</button>
+    </div>
+  );
+}
+
+export default Home;
